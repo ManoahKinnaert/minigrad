c3d81832f7880f5079a9318fd1ff063f9f1522c5
Manoah Kinnaert <manoah.k@icloud.com>
2025-07-29 16:31:27 +0200

    added sum mean and logsoftmax

--- minigrad/function.py
+++ minigrad/function.py
@@ -57,37 +57,68 @@ class Relu(Function):
         ctx = c.Context()
         ctx.save_for_backward(input)
         ctx.function = Relu
-        return t.Tensor(np.maximum(0, input), ctx=ctx)
+        return t.Tensor(np.maximum(0, input.data), ctx=ctx)
 
     @staticmethod
     def backward(ctx: c.Context, grad_out):
-        out = ctx._prev 
-        return (out.data > 0) * grad_out 
+        input, = ctx._prev 
+        return (input.data > 0) * grad_out 
     
 
 class Sigmoid(Function):
     @staticmethod
     def forward(input):
+        sig = 1 / (1 * np.exp(-input.data))
         ctx = c.Context()
-        ctx.save_for_backward(input)
+        ctx.save_for_backward(t.Tensor(sig))
         ctx.function = Sigmoid
-        return t.Tensor(1 / (1 * np.exp(-input.data)))
+        return t.Tensor(sig, ctx=ctx)
 
     @staticmethod 
     def backward(ctx: c.Context, grad_out):
-        out = ctx._prev 
-        return (out.data * (1 - out.data)) * grad_out 
+        sig, = ctx._prev 
+        return grad_out * sig.data * (1 - sig.data)
 
 class Tanh(Function):
     @staticmethod 
     def forward(input):
+        tanh_val = np.tanh(input.data)
         ctx = c.Context()
-        ctx.save_for_backward(input)
+        ctx.save_for_backward(t.Tensor(tanh_val))
         ctx.function = Tanh
-        return t.Tensor(np.tanh(input.data), ctx=ctx)
+        return t.Tensor(tanh_val, ctx=ctx)
 
     @staticmethod 
     def backward(ctx: c.Context, grad_out):
-        out = ctx._prev 
-        return (1 - out.data ** 2) * grad_out 
+        tanh_val, = ctx._prev 
+        return (1 - tanh_val.data ** 2) * grad_out 
+
+class Sum(Function):
+    @staticmethod
+    def forward(input):
+        ctx = c.Context()
+        ctx.save_for_backward(input)
+        ctx.function = Sum 
+        return t.Tensor(input.data.sum(), ctx=ctx)
     
+    @staticmethod
+    def backward(ctx: c.Context, grad_out):
+        input, = ctx._prev
+        return grad_out * np.ones_like(input.data)
+    
+class LogSoftmax(Function):
+    @staticmethod
+    def forward(input: t.Tensor):
+        ctx = c.Context()
+        c_max = input.data.max(axis=1, keepdims=True)
+        logsumexp = c_max + np.log(np.sum(np.exp(input.data - c_max), axis=1, keepdims=True))
+        output = input.data - logsumexp
+        ctx.save_for_backward(t.Tensor(output))
+        ctx.function = LogSoftmax
+        return t.Tensor(output, ctx=ctx)
+
+    @staticmethod
+    def backward(ctx: c.Context, grad_out):
+        (log_softmax,) = ctx._prev
+        softmax = np.exp(log_softmax.data)
+        return grad_out - softmax * np.sum(grad_out, axis=1, keepdims=True)
--- minigrad/tensor.py
+++ minigrad/tensor.py
@@ -8,6 +8,9 @@ class Tensor:
         # context used for autograd graph construction
         self.ctx = ctx
 
+    def __repr__(self):
+        return f"Tensor(data={self.data}, grad={self.grad})"
+    
     ##### ops ##### 
     def __add__(self, other):
         if not isinstance(other, Tensor):
@@ -18,6 +21,16 @@ class Tensor:
         if not isinstance(other, Tensor):
             other = Tensor(other)
         return f.Mul.forward(self, other)
+    
+    def __sub__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+        return self + (other * -1)
+
+    def __truediv__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+        return self * Tensor(1.0 / other.data)
     
     def dot(self, other):
         if not isinstance(other, Tensor):
@@ -32,6 +45,15 @@ class Tensor:
     
     def tanh(self):
         return f.Tanh.forward(self)
+    
+    def sum(self):
+        return f.Sum.forward(self)
+    
+    def mean(self):
+        return self.sum() * (1.0 / self.data.size)
+    
+    def logsoftmax(self):
+        return f.LogSoftmax.forward(self)
 
     def backward(self, grad_out=None):
         if grad_out is None:
@@ -44,11 +66,17 @@ class Tensor:
             return 
         
         grads = self.ctx.function.backward(self.ctx, grad_out)
+        if not isinstance(grads, (tuple, list)):
+            grads = (grads,)
         for tensor, grad in zip(self.ctx._prev, grads):
             tensor.backward(grad)
+    
 
+
+    @staticmethod
     def randn(*args, **kwargs):
         return Tensor(data=np.random.randn(*args, **kwargs))
     
+    @staticmethod
     def zeros(*args, **kwargs):
         return Tensor(data=np.zeros(*args, **kwargs))
