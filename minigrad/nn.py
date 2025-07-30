b5cb5da89ec387a780bf2db5f95b62551a0f08de
Manoah Kinnaert <manoah.k@icloud.com>
2025-07-30 14:52:27 +0200

    Added (very basic) loss + train

--- minigrad/nn.py
+++ minigrad/nn.py
@@ -2,14 +2,6 @@ from minigrad.tensor import Tensor
 from minigrad.function import *
 import numpy as np
 
-class Parameter(Tensor):
-    def __new__(cls, data=None):
-        if data is None:
-            data = np.empty(0, dtype=float)
-        super().__init__(data)
-
-    def __repr__(self):
-        return f"Parameter containing: {super().__repr__()}"
    
 class Module:
     def zero_grad(self):
@@ -41,17 +33,49 @@ class Layer(Module):
             return self.activation.forward(l)
         return l
 
+class Loss(Module):
+    def calculate(self, *args, **kwargs) -> Tensor:
+        raise NotImplementedError("To implement loss you must define how to calculate the loss.") 
+
+class MeanSquared(Loss):
+    def calculate(self, pred, y) -> Tensor:
+        return ((pred - y) ** 2).mean()
+
 class Model(Module):
     def __init__(self):
         self.layers = []
+        self.X = None 
+        self.y = None 
+        self.loss_function: Loss = MeanSquared   # default to mean squared loss 
+        self.optim = None   # no optimizer by default -> will change this in the future
 
     def add_layer(self, nin: int, nout: int, activation: Function=None, **kwargs):
         self.layers.append(Layer(nin, nout, activation))
 
     def forward(self):
+        pred = self.X
         for layer in self.layers:
-            layer.forward()
+            pred = layer.forward()
+        return pred 
+    
+    # TODO: Optimizer to still be implemented and multi batch too
+    def train(self, lr, epochs, batch=1, debug=False):
+        for epoch in range(epochs):
+            # forward pass + loss
+            pred = self.forward()
+            loss = self.loss_function.calculate(pred, self.y)
+            # run through backwards pass
+            loss.backward()
+            # update params
+            for layer in self.layers:
+                layer.w.data -= lr * layer.w.grad 
+                layer.b.data -= lr * layer.b.grad
+            # Reset
+            for layer in self.layer:
+                layer.w.zero_grad()
+                layer.b.zero_grad()
 
-    # TODO: Loss to still be implemented 
-    def backward(self, loss):
-        pass 
+            # print the loss and TODO: other useful things
+            if debug:
+                print(f"Epoch: {epoch}: loss = {loss.data:.6f}")
+            
