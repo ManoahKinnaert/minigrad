decaaff1e2fa21b8c59f96783948b25de526e25c
Manoah Kinnaert <manoah.k@icloud.com>
2025-07-30 15:10:18 +0200

    Better imports

--- minigrad/function.py
+++ minigrad/function.py
@@ -1,7 +1,6 @@
 import numpy as np
-import minigrad.tensor as t
-import minigrad.context as c
-
+from minigrad.tensor import Tensor 
+from minigrad.context import Context 
 class Function:
     @staticmethod
     def forward(*args, **kwargs):
@@ -14,53 +13,53 @@ class Function:
 ## some basic functions
 class Add(Function):
     @staticmethod
-    def forward(x: t.Tensor, y: t.Tensor):
-        ctx = c.Context()
+    def forward(x: Tensor, y: Tensor):
+        ctx = Context()
         ctx.save_for_backward(x, y)
         ctx.function = Add
-        return t.Tensor(x.data + y.data, ctx=ctx) 
+        return Tensor(x.data + y.data, ctx=ctx) 
     
     @staticmethod
-    def backward(ctx: c.Context, grad_out):
+    def backward(ctx: Context, grad_out):
         x, y = ctx._prev 
         return grad_out, grad_out 
     
 class Mul(Function):
     @staticmethod
-    def forward(x: t.Tensor, y: t.Tensor):
-        ctx = c.Context()
+    def forward(x: Tensor, y: Tensor):
+        ctx = Context()
         ctx.save_for_backward(x, y)
         ctx.function = Mul 
-        return t.Tensor(x.data * y.data, ctx=ctx)
+        return Tensor(x.data * y.data, ctx=ctx)
     
     @staticmethod
-    def backward(ctx: c.Context, grad_out):
+    def backward(ctx: Context, grad_out):
         x, y = ctx._prev 
         return y.data * grad_out, x.data * grad_out 
 
 class Dot(Function):
     @staticmethod
-    def forward(x: t.Tensor, y: t.Tensor):
-        ctx = c.Context()
+    def forward(x: Tensor, y: Tensor):
+        ctx = Context()
         ctx.save_for_backward(x, y)
         ctx.function = Dot 
-        return t.Tensor(np.dot(x.data, y.data), ctx=ctx)
+        return Tensor(np.dot(x.data, y.data), ctx=ctx)
 
     @staticmethod 
-    def backward(ctx: c.Context, grad_out):
+    def backward(ctx: Context, grad_out):
         x, y = ctx._prev 
         return np.dot(grad_out, y.data.T), np.dot(x.data.T, grad_out)
 
 class Pow(Function):
     @staticmethod
     def forward(base, power):
-        ctx = c.Context()
+        ctx = Context()
         ctx.save_for_backward(base, power)
         ctx.function = Pow
-        return t.Tensor(np.power(base.data, power.data), ctx=ctx)
+        return Tensor(np.power(base.data, power.data), ctx=ctx)
 
     @staticmethod
-    def backward(ctx: c.Context, grad_out):
+    def backward(ctx: Context, grad_out):
         base, power = ctx._prev
         grad_base = power.data * np.power(base.data, power.data - 1) * grad_out
         grad_power = np.power(base.data, power.data) * np.log(base.data) * grad_out
@@ -69,13 +68,13 @@ class Pow(Function):
 class Relu(Function):
     @staticmethod
     def forward(input):
-        ctx = c.Context()
+        ctx = Context()
         ctx.save_for_backward(input)
         ctx.function = Relu
-        return t.Tensor(np.maximum(0, input.data), ctx=ctx)
+        return Tensor(np.maximum(0, input.data), ctx=ctx)
 
     @staticmethod
-    def backward(ctx: c.Context, grad_out):
+    def backward(ctx: Context, grad_out):
         input, = ctx._prev 
         return (input.data > 0) * grad_out 
     
@@ -83,13 +82,13 @@ class Sigmoid(Function):
     @staticmethod
     def forward(input):
         sig = 1 / (1 + np.exp(-input.data))
-        ctx = c.Context()
-        ctx.save_for_backward(t.Tensor(sig))
+        ctx = Context()
+        ctx.save_for_backward(Tensor(sig))
         ctx.function = Sigmoid
-        return t.Tensor(sig, ctx=ctx)
+        return Tensor(sig, ctx=ctx)
 
     @staticmethod 
-    def backward(ctx: c.Context, grad_out):
+    def backward(ctx: Context, grad_out):
         sig, = ctx._prev 
         return grad_out * sig.data * (1 - sig.data)
 
@@ -97,42 +96,42 @@ class Tanh(Function):
     @staticmethod 
     def forward(input):
         tanh_val = np.tanh(input.data)
-        ctx = c.Context()
-        ctx.save_for_backward(t.Tensor(tanh_val))
+        ctx = Context()
+        ctx.save_for_backward(Tensor(tanh_val))
         ctx.function = Tanh
-        return t.Tensor(tanh_val, ctx=ctx)
+        return Tensor(tanh_val, ctx=ctx)
 
     @staticmethod 
-    def backward(ctx: c.Context, grad_out):
+    def backward(ctx: Context, grad_out):
         tanh_val, = ctx._prev 
         return (1 - tanh_val.data ** 2) * grad_out 
 
 class Sum(Function):
     @staticmethod
     def forward(input):
-        ctx = c.Context()
+        ctx = Context()
         ctx.save_for_backward(input)
         ctx.function = Sum 
-        return t.Tensor(input.data.sum(), ctx=ctx)
+        return Tensor(input.data.sum(), ctx=ctx)
     
     @staticmethod
-    def backward(ctx: c.Context, grad_out):
+    def backward(ctx: Context, grad_out):
         input, = ctx._prev
         return grad_out * np.ones_like(input.data)
     
 class LogSoftmax(Function):
     @staticmethod
-    def forward(input: t.Tensor):
-        ctx = c.Context()
+    def forward(input: Tensor):
+        ctx = Context()
         c_max = input.data.max(axis=1, keepdims=True)
         logsumexp = c_max + np.log(np.sum(np.exp(input.data - c_max), axis=1, keepdims=True))
         output = input.data - logsumexp
-        ctx.save_for_backward(t.Tensor(output))
+        ctx.save_for_backward(Tensor(output))
         ctx.function = LogSoftmax
-        return t.Tensor(output, ctx=ctx)
+        return Tensor(output, ctx=ctx)
 
     @staticmethod
-    def backward(ctx: c.Context, grad_out):
+    def backward(ctx: Context, grad_out):
         (log_softmax,) = ctx._prev
         softmax = np.exp(log_softmax.data)
         return grad_out - softmax * np.sum(grad_out, axis=1, keepdims=True)
