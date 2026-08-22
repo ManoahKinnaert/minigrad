fab66a7f75f19b86b52e65fcdd2fbce9242101e2
Manoah Kinnaert <manoah.k@icloud.com>
2026-08-22 21:03:32 +0200

    minidoc

--- minigrad/nn/model.py
+++ minigrad/nn/model.py
@@ -9,8 +9,13 @@ import minigrad.nn.optim as optim
 from tqdm import trange
 
 class Model(Module):
+    """
+    A model class to allow the client to create a neural network model.
+    The client may enter X and y tensors used for training, but this may also be accomplished by using the set_training_data method.
+    Furthermore it allows the client to define a loss type and an optimizer of choice.
+    """
     def __init__(self, X: Tensor=None, y: Tensor=None, loss: Loss=MeanSquared, optimizer: optim.Optimizer=optim.SGD):
-        self.layers = []
+        self._layers = []
         self.X: Tensor = X 
         self.y: Tensor = y 
         self.loss_function: Loss = loss  # default to mean squared loss 
@@ -18,30 +23,69 @@ class Model(Module):
         self.optim = None
 
     @property
-    def n_samples(self): return 0 if self.X is None else self.X.data.shape[0]
+    def n_samples(self): 
+        """
+        Return num of samples, in the case that self.X is None we return 0.
+        """
+        return 0 if self.X is None else self.X.data.shape[0]
 
     @property 
-    def parameters(self): return [p for layer in self.layers for p in layer.parameters]
+    def parameters(self): 
+        """
+        Returns the layer params.
+        """
+        return [p for layer in self._layers for p in layer.parameters]
+
+    @property 
+    def layers(self): 
+        """
+        Return a copy of the internal layers.
+        """
+        return self._layers.copy()
+
+    @property
+    def n_layers(self):
+        """
+        Return the amount of layers.
+        """
+        return len(self._layers)
 
     def create_layer(self, nin: int, nout: int, activation: Function=None, **kwargs):
+        """
+        Create a new layer and add it to the internal layers representation.
+        """
         self.layers.append(Layer(nin, nout, activation))
 
     def set_training_data(self, X: Tensor, y: Tensor):
+        """
+        Set the training data.
+        """
         self.X = X 
         self.y = y 
 
     def clear_training_data(self):
+        """
+        Clear out the training data (hoping to save some memory for larger models).
+        """
         self.X = None 
         self.y = None
 
     def forward(self, X: Tensor):
+        """
+        Implementation of the forward pass.
+        """
         pred = X
-        for layer in self.layers:
+        for layer in self._layers:
             pred = layer.forward(pred)
         return pred 
     
-    # TODO: properly implement batching
     def train(self, lr, epochs, batch_size=1, debug=False):
+        """
+        The train method which is called to train the model.
+        The client may set the learning rate named lr, the epochs named epochs,
+        the batch size, not amount of batches, named batch_size and may choose to train in debug mode.
+        Debug mode displays amount of epochs passed and loss, it (debug) is set to False by default
+        """
         if self.X is None: raise ValueError("Training data has not been set: (at least) X is missing!")
         if self.y is None: raise ValueError("Training data has not been set: y is missing!")
 
@@ -74,6 +118,10 @@ class Model(Module):
         self.clear_training_data()
 
     def train_on_data(self, X: Tensor, y: Tensor, lr, epochs, batch=1, debug=False):
+        """
+        Method for training, basically identical to train but just to allow a more 'flexible' signature. We allow
+        the client to pass the training data directly at the same time as training.
+        """
         self.set_training_data(X, y)
         self.train(lr, epochs, batch, debug)
         self.clear_training_data()
