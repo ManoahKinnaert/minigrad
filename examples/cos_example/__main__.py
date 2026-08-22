92bbe5452eb5dfbc8633d2f5044fb89db0dbb81e
Manoah Kinnaert <manoah.k@icloud.com>
2026-08-22 21:25:57 +0200

    Add cosine fitting example

---
+++ examples/cos_example/__main__.py
@@ -0,0 +1,33 @@
+from minigrad import Tensor 
+from minigrad.core import function as f 
+from minigrad.nn import Model
+from minigrad.nn import optim
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# create training data
+np.random.seed(0)
+
+X = Tensor(np.linspace(-2 * np.pi, 2 * np.pi, 200).reshape(-1, 1))
+y = Tensor(np.cos(X.data))
+
+# define a model
+model = Model(X, y, optimizer=optim.Adam)
+model.create_layer(nin=1, nout=64, activation=f.Relu)
+model.create_layer(nin=64, nout=64, activation=f.Relu)
+model.create_layer(nin=64, nout=1)
+
+def plot_fitted_curve():
+    y_pred = model.forward(X)
+
+    # plot 
+    fix, ax = plt.subplots()
+    ax.plot(X.data, y.data, label="Target cos", lw=4)
+    ax.plot(X.data, y_pred.data, label="Prediction", lw=2)
+    ax.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    model.train(lr=0.01, batch_size=64, epochs=10000, debug=True)
+    plot_fitted_curve()
