d1a5359db165167544995b725d78f322abf50bad
Manoah Kinnaert <manoah.k@icloud.com>
2026-08-24 09:52:13 +0200

    Start work on testing static methods

--- tests/core/test_tensor.py
+++ tests/core/test_tensor.py
@@ -2,12 +2,29 @@
 This file houses unittests for the minigrad.core.Tensor class.
 """
 import unittest
+import numpy as np
 from minigrad import Tensor
 
 class TensorTest(unittest.TestCase):
     def test_init_value_error(self):
         """Test data Nonetype""" 
         self.assertRaises(ValueError, lambda: Tensor(None, None))
+
+    # test static methods
+    def test_randn_1d_length5(self):
+        self.assertEqual(5, Tensor.randn(5).data.size)
+
+    def test_randn_1d_length100(self):
+        self.assertEqual(100, Tensor.randn(100).data.size)
+
+    def test_zeros_1d_length5(self):
+        np.testing.assert_array_equal(np.zeros(5), Tensor.zeros(5).data)
+
+    def test_ones(self):
+        pass 
+
+    def test_ones_like(self):
+        pass 
 
 if __name__ == "__main__":
     unittest.main()
