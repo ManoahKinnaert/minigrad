9c4e616f7e47c0ea4649bac21119f6f9102302af
Manoah Kinnaert <manoah.k@icloud.com>
2026-08-26 09:03:58 +0200

    Added some tests

--- tests/nn/test_layer.py
+++ tests/nn/test_layer.py
@@ -2,19 +2,20 @@
 This file contains the unittests for the minigrad.nn.Layer class
 """
 import unittest
+from minigrad.nn import Layer
 
 class LayerTest(unittest.TestCase):
     def test_init_nin_is_none(self):
-        pass 
+        self.assertRaises(ValueError, lambda: Layer(None, 1))
 
     def test_init_nin_is_smaller_than_1(self):
-        pass 
+        self.assertRaises(ValueError, lambda: Layer(0, 1))
 
     def test_init_nout_is_none(self):
-        pass 
+        self.assertRaises(ValueError, lambda: Layer(1, None))
 
     def test_init_nout_is_smaller_than_1(self):
-        pass 
+        self.assertRaises(ValueError, lambda: Layer(1, 0))
 
 if __name__ == "__main__":
     unittest.main()
