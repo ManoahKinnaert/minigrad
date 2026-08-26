562a6693d927972fc8b2bf8e609569d46fe185d0
Manoah Kinnaert <manoah.k@icloud.com>
2026-08-26 08:58:00 +0200

    basic init method safety skeleton

--- tests/nn/test_layer.py
+++ tests/nn/test_layer.py
@@ -4,7 +4,17 @@ This file contains the unittests for the minigrad.nn.Layer class
 import unittest
 
 class LayerTest(unittest.TestCase):
-    pass 
+    def test_init_nin_is_none(self):
+        pass 
+
+    def test_init_nin_is_smaller_than_1(self):
+        pass 
+
+    def test_init_nout_is_none(self):
+        pass 
+
+    def test_init_nout_is_smaller_than_1(self):
+        pass 
 
 if __name__ == "__main__":
     unittest.main()
